09ce745dda0cd1cbd0171b4b35f02fab0f87b563
textworld <text.zwb@gmail.com>
2021-09-18 16:07:31 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/textworld/go_module_t
+
+go 1.16
---
+++ sub.go
@@ -0,0 +1,10 @@
+/**
+ * @Author: zhangwenbing@u51-inc.com
+ * @Date: 2021/9/18 下午4:02
+ * @Desc:
+ */
+package main
+
+func Sub(a, b int) int {
+	return a - b
+}
